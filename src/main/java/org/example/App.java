952180378218aa935c35f9647cffe4add275a706
Justyna Adamska <110631335+Justyna-Adamska@users.noreply.github.com>
2023-04-20 19:45:53 +0200

    done cities

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Girls!" );
+//        System.out.println( "Hello Monika :)" );
+//        System.out.println( "Hello Girls!" );
+        String [] cities = new String [5];
+
+        cities[0]="Paryż";
+        cities[1]="Madryt";
+        cities[2]="Porto";
+        cities[3]="Barcelona";
+        cities[4]="Londyn";
+
+        for (int i=0; i< cities.length;i++){
+            String city = cities[i];
+            System.out.println(city);
+
+        }
+        for (int j=4; j>=0; j--){
+            String city = cities[j];
+            System.out.println(city);
+        }
+        for (int i=0; i< cities.length;i+=2) {
+            String city = cities[i];
+            System.out.println(city);
+        //    System.out.println(((i+1)+ ". "+city));
+        }
     }
 }
